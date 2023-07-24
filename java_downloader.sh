34276c716b3a76dbfdc5be004b53c94a694b79aa
Aly Ghazal <ali.ahmed2775@gmail.com>
2023-07-24 13:25:47 +0300

    .profile instead of .bashrc

--- java_downloader.sh
+++ java_downloader.sh
@@ -8,6 +8,7 @@ echo "this user is already exist"
 else
 sudo useradd -m -d /home/pet-clinic -c "pet clinic" -s /bin/bash  pet-clinic
 echo -e "$pet_pass\n$pet_pass"|sudo passwd pet-clinic
+sudo usermod -aG sudo pet-clinic
 #sudo chmod 777 /home/pet-clinic
 #sudo chmod 777  /home/pet-clinic/.bashrc
 fi
@@ -23,7 +24,9 @@ else
 curl -O https://download.java.net/java/GA/jdk18/43f95e8614114aeaa8e8a5fcf20a682d/36/GPL/openjdk-18_linux-x64_bin.tar.gz
 #cp ./openjdk-18_linux-x64_bin.tar.gz  /home/pet-clinic
 tar -xvf /home/pet-clinic/openjdk-18_linux-x64_bin.tar.gz
-echo JAVA_HOME="/home/pet-clinic/jdk-18" >> ./.bashrc
-echo PATH="/home/pet-clinic/jdk-18/bin:$PATH" >> ./.bashrc
+echo JAVA_HOME="/home/pet-clinic/jdk-18" >> ./.profile
+echo PATH="/home/pet-clinic/jdk-18/bin:$PATH" >> ./.profile
 fi
+git clone https://github.com/spring-projects/spring-petclinic.git
 EOF
+
